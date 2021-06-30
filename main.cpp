fe5642ebb11e5b86dd15e573d27b32bbb3768088
WINDOWS-6SNMTFQ\Usuario <lpushug@est.ups.edu.ec>
2021-06-30 13:01:01 -0500

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\t;D Hola Mundo :D"
+            "\nMi nombre es Liseth"
+            "\nSoy estudiante de Ingenieria en Computacion"
+            ", y estoy probando el Git :v\n\n\n";
+    return 0;
+}
